5285977b6f2b631d22792a8c08cf6a6914536be6
Kira1922 <kirap996@gmail.com>
2025-07-26 12:45:09 +0300

    Добавлены кнопка карта

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
+import webbrowser
 
 def get_coordinates(city, key):
     try:
@@ -9,21 +10,36 @@ def get_coordinates(city, key):
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
             country = results[0]['components']['country']
+            osm_url = f"https://www.openstreetmap.org/?mlat={lat}&mlon={lon}"
 
             if 'state' in results[0]['components']:
                 region = results[0]['components']['state']
-                return f"Широта: {lat}, Долгота: {lon}\n Страна: {country}. Регион: {region}"
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lon}\nСтрана: {country}\nРегион: {region}",
+                    "map_url": osm_url
+                        }
             else:
-                return f"Широта: {lat}, Долгота: {lon}\nСтрана: {country}"
+                return {
+                    "coordinates": f"Широта: {lat}, Долгота: {lon}\nСтрана: {country}\nРегион: {region}",
+                    "map_url": osm_url
+                        }
         else:
             return "Город не найден"
     except Exception as e:
         return f"Общая ошибка: {e}"
 
+
 def show_coordinates(event=None):
     city = entry.get()
-    coordinates = get_coordinates(city, key)
-    label.config(text=f"Координаты города {city}:\n {coordinates}")
+    result = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n {result["coordinates"]}")
+    global map_url
+    map_url = result["map_url"]
+
+def show_map():
+    if map_url:
+        webbrowser.open(map_url)
+
 
 
 
@@ -43,5 +59,9 @@ button.pack()
 
 label = Label(text="Введите город и нажмите Поиск")
 label.pack()
+
+map_button = Button(text="Показать карту", command=show_map)
+map_button.pack()
+
 
 window.mainloop()
